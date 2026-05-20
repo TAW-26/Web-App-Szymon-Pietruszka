93b6e89d48dfcf57206634d5b2e8a5143916e9d9
captainSaymon <szymonpietruszka@onet.pl>
2026-05-21 00:08:46 +0200

    odpalenie servera

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,27 @@
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine, text
+from fastapi import FastAPI, Depends
 from app.config import DATABASE_URL
+
+app = FastAPI()
 
 try:
     engine = create_engine(DATABASE_URL)
-    
-    with engine.connect() as connection:
-        result = connection.execute(text("SELECT 1"))
-        print("Połączenie z bazą danych działa prawidłowo")
+
+    SessionLocal = sessionmaker(autoflush=False, bind=engine)
+    print("Połączenie z bazą danych działa prawidłowo")
         
 except Exception as e:
     print(f"Nie udało się połączyć z bazą danych: {e}")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Serwer FastAPI działa lokalnie!"}
