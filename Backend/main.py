68581f0ace22d707305792fa9f295e590891ac78
captainSaymon <szymonpietruszka@onet.pl>
2026-05-21 20:45:50 +0200

    optymalizacja pliku głównego

--- Backend/main.py
+++ Backend/main.py
@@ -1,27 +1,5 @@
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import create_engine, text
-from fastapi import FastAPI, Depends
-from app.config import DATABASE_URL
+from src.rest import router
+from fastapi import FastAPI
 
 app = FastAPI()
-
-try:
-    engine = create_engine(DATABASE_URL)
-
-    SessionLocal = sessionmaker(autoflush=False, bind=engine)
-    print("Połączenie z bazą danych działa prawidłowo")
-        
-except Exception as e:
-    print(f"Nie udało się połączyć z bazą danych: {e}")
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.get("/")
-def read_root():
-    return {"message": "Serwer FastAPI działa lokalnie!"}
+app.include_router(router)
