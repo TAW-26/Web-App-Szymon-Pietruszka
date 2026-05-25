44b051a05c660b7190189db5b18c67f9126bd63d
captainSaymon <szymonpietruszka@onet.pl>
2026-05-25 20:28:35 +0200

    poprawki oraz pobieranie filmów wraz z gatunkami

--- Backend/src/rest.py
+++ Backend/src/rest.py
@@ -1,8 +1,11 @@
 from fastapi import Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from fastapi import APIRouter
+from typing import List
+
 from src.database.connect import engine, get_db
 from src.model import models
+from src.model import structure
 
 router  = APIRouter()
 
@@ -18,20 +21,20 @@ def get_users(db: Session = Depends(get_db)):
     return users
 
 @router.get("/user/{id}")
-def get_users(id: int, db: Session = Depends(get_db)):
+def get_user_by_id(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id_user == id).first()
     if not user:
             raise HTTPException(status_code=404, detail="User not found")
     
     return user
 
-@router.get("/movies")
-def get_users(db: Session = Depends(get_db)):
-    movies = db.query(models.Movie).all()
+@router.get("/movies", response_model=List[structure.MovieResponseSchema])
+def get_movies(db: Session = Depends(get_db)):
+    movies = db.query(models.Movie).options(joinedload(models.Movie.genres)).all()
     return movies
 
 @router.get("/movie/{id}")
-def get_users(id: int, db: Session = Depends(get_db)):
+def get_movie_by_id(id: int, db: Session = Depends(get_db)):
     movie = db.query(models.Movie).filter(models.Movie.id_movie == id).first()
     if not movie:
             raise HTTPException(status_code=404, detail="Movie not found")
