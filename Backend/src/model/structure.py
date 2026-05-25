f4bb686a32a4bfd60318796b32e3c9f630c7f3b1
captainSaymon <szymonpietruszka@onet.pl>
2026-05-25 20:28:11 +0200

    struktura zwrotynych danych

---
+++ Backend/src/model/structure.py
@@ -0,0 +1,27 @@
+from typing import List
+from pydantic import BaseModel, field_validator
+
+class MovieResponseSchema(BaseModel):
+    id_movie: int
+    title: str
+    description: str
+    director: str
+    composer: str
+    poster: str | None = None
+    production: str
+    year: int
+    rating: float
+    genres: List[str]
+    actors: List[str] | None
+
+    class Config:
+        from_attributes = True
+
+    @field_validator("genres", mode="before")
+    @classmethod
+    def serialize(cls, value):
+        if isinstance(value, list):
+            return [g.name if hasattr(g, "name") else g.get("name") for g in value]
+        
+        return value
+    
