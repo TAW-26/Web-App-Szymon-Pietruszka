cc42edc2589309d6a9a91a5afaf5cb3de48c52d3
captainSaymon <szymonpietruszka@onet.pl>
2026-06-01 23:57:51 +0200

    prosty serwis z wiadomością

---
+++ Frontend/src/app/services/api.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { config } from '../config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = `${config.url}/`
+
+  constructor(private http: HttpClient) {}
+
+  getMessage(): Observable<{ message: string }> {
+    return this.http.get<{ message: string }>(this.apiUrl)
+  }
+}
