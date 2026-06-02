19846f4472ac22a6134b536963ea1aa570aacb8a
captainSaymon <szymonpietruszka@onet.pl>
2026-06-02 20:54:09 +0200

    od nowa

--- Frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Movies } from './components/movies/movies';
-import { Welcome } from './components/welcome/welcome';
-
-export const routes: Routes = [
-    { path: '', component: Welcome },
-    { path: 'discover', component: Movies },
-];
--- Frontend/src/app/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-    port: process.env['PORT'] || 8000, 
-    url: process.env['URL'] || 'http://localhost:8000' 
-}
