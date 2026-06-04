19b4190a674c29735686def0ad69edacc00c09e8
captainSaymon <szymonpietruszka@onet.pl>
2026-06-04 19:11:10 +0200

    poprawka folderu

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { Start } from './components/start/start';
 import { Movies } from './components/movies/movies';
-import { Join } from './components/user/join/join';
+import { Join } from './components/user/login.register/join';
 
 export const routes: Routes = [
     { path: '', component: Start},
