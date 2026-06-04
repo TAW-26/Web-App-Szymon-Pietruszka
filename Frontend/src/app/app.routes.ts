37bfa50877fb5b0d2ef1bc9c788bb978504d17f4
captainSaymon <szymonpietruszka@onet.pl>
2026-06-04 23:52:52 +0200

    obsługa błędów

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { UserData } from './components/user/user.data/user.data';
 import { Favorites } from './components/favorites/favorites';
 import { Activity } from './components/activity/activity';
 import { authGuard } from './services/auth/auth-guard';
+import { NotFound } from './components/errors/not-found/not-found';
+import { ServerError } from './components/errors/server-error/server-error';
 
 export const routes: Routes = [
     { path: '', component: Start},
@@ -14,4 +16,6 @@ export const routes: Routes = [
     { path: 'favorite', component: Favorites, canActivate: [authGuard]},
     { path: 'account', component: UserData, canActivate: [authGuard] },
     { path: 'login-register', component: Join },
+    { path: 'server-error', component: ServerError }, 
+    { path: '**', component: NotFound },
 ];
