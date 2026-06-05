171646b0904004de6810c35b51caf2144812efcf
captainSaymon <szymonpietruszka@onet.pl>
2026-06-05 15:07:29 +0200

    usuwanie filmów z ulubionych

--- Frontend/src/app/components/favorites/favorites.ts
+++ Frontend/src/app/components/favorites/favorites.ts
@@ -1,18 +1,21 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { UserFavoritesResponse } from '../../services/models/data.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './favorites.html',
   styleUrl: './favorites.scss',
 })
 export class Favorites {
   userFavorites: UserFavoritesResponse | null = null;
+  favoriteMessage: string = '';
+  isCooldown: boolean = false;
 
-  constructor(private authService: AuthService, private cdr: ChangeDetectorRef) {}
+  constructor(public authService: AuthService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.loadUserFavorites();
@@ -28,5 +31,42 @@ export class Favorites {
         console.error("Failed to load user favorites: ", err);
       }
     })
+  }
+
+  favortieDelete(id: number) {
+    if (this.isCooldown) return;
+
+    this.isCooldown = true;
+
+    this.authService.deleteFavorite(id).subscribe({
+      next: (data) => {
+        console.log("delete from favortie")
+
+        if (this.userFavorites && this.userFavorites.favorite) {
+          this.userFavorites.favorite = this.userFavorites.favorite.filter(
+            (movie: any) => movie.id_movie !== id
+          );
+        }
+
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.log('Error:', err.status);
+        
+        if (err.status === 404) {
+          this.favoriteMessage = 'Film został już usunięty dodany';
+        }
+        else if (err.status === 401) {
+          this.favoriteMessage = 'Nie poprawny JWT';
+        }
+        else {
+          this.favoriteMessage = 'Something goes wrong. I can feel it';
+        }
+      }
+    });
+
+    setTimeout(() => {
+      this.isCooldown = false;
+    }, 2000);
   }
 }
