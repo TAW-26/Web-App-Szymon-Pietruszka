a44958d41d654609fa605e28219a112de8bc4467
captainSaymon <szymonpietruszka@onet.pl>
2026-06-05 18:11:30 +0200

    pole w nawigasji do szukania

--- Frontend/src/app/components/nav/nav.ts
+++ Frontend/src/app/components/nav/nav.ts
@@ -1,17 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
+import { Search } from '../../services/search/search';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.scss',
 })
-export class Nav {
-  constructor(public authService: AuthService) {}
+export class Nav implements OnInit{
+  searchControl = new FormControl('');
+
+  constructor(public authService: AuthService, private search: Search) {}
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges.subscribe(value => {
+      this.search.setSearch(value || '');
+    });
+  }
 
   onLogout() {
     this.authService.logout();
