07f0b7cdc7c67fb7e7c12fcf3d5ea4eb5e2735f1
captainSaymon <szymonpietruszka@onet.pl>
2026-06-06 16:08:36 +0200

    test logowania

--- Backend/test/test_api.py
+++ Backend/test/test_api.py
@@ -1,9 +1,10 @@
 import httpx
 
 BASE_URL = "http://localhost:8000"
+JWT = ''
 
 
-def test_read_root_title():
+def test_read_title():
     # Arrange
     url = f"{BASE_URL}/"
     expected_title = "MovieCheck"
@@ -13,3 +14,21 @@ def test_read_root_title():
 
     # Assert
     assert response.json()["title"] == expected_title
+
+def test_login():
+    # Arrange
+    url = f"{BASE_URL}/login"
+    payload = {
+        "nickname": "szymon",
+        "password": "szymon"
+    }
+
+    # Act
+    response = httpx.post(url, json=payload)
+    data = response.json()
+
+    # Load JWT for other tests
+    JWT = data["access_token"]
+
+    # Assert
+    assert response.status_code == 200
