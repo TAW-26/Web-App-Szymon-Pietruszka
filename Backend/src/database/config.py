1610dfc598464e84fb0010ac021087edef1678f8
captainSaymon <szymonpietruszka@onet.pl>
2026-06-07 23:28:02 +0200

    poprawki związane z .env

--- Backend/src/database/config.py
+++ Backend/src/database/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
-host = os.getenv('DB_HOST')
-port = os.getenv('DB_PORT')
+host = os.getenv('DB_HOST', 'postgres_db')
+port = os.getenv('DB_PORT', '5432')
 db = os.getenv('DB_NAME')
 
 DATABASE_URL = f'postgresql://{user}:{password}@{host}:{port}/{db}'
